8b7b8daabfeaf23c1338fc4bc51b5e2b8ae5be28
Steven Young <stevenyoung131@gmail.com>
2017-03-24 09:54:48 -0400

    Fix PEP8 errors

--- media.py
+++ media.py
@@ -1,10 +1,15 @@
 import webbrowser
 
+
 class Movie():
     '''This class provides a way to store movie related information'''
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, directed_by, movie_rating):
-        '''This constructor method initializes an instance of the class Movie, taking inputs including title, storyline, a poster image, youtube trailer url, director name, and rating, and will output those values to the instance variables listed below.'''
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, directed_by, movie_rating):
+        '''This constructor method initializes an instance of the class Movie,
+        taking inputs including title, storyline, a poster image, youtube
+        trailer url, director name, and rating, and will output those values
+        to the instance variables listed below.'''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -13,5 +18,6 @@ class Movie():
         self.directed_by = directed_by
 
     def show_trailer(self):
-        '''This function will open a browswer window, and navigate to the URL specified by the instance variable trailer_youtube_url'''
+        '''This function will open a browswer window, and navigate to the URL
+         specified by the instance variable trailer_youtube_url'''
         webbrowser.open(self.trailer_youtube_url)
